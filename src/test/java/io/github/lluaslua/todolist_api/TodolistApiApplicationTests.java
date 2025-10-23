1ea2de432d4d1837edcd73ea6defe2204d7e5f42
Luan Gouveia <luangfdev@gmail.com>
2025-10-23 15:50:38 -0300

    commit inicial springboot

---
+++ src/test/java/io/github/lluaslua/todolist_api/TodolistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lluaslua.todolist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
