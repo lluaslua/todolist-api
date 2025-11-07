655d2d8ceb278ad4f94f8de45601e67212f06cdd
Luan Gouveia <luangfdev@gmail.com>
2025-11-07 10:57:59 -0300

    tasksService

---
+++ src/main/java/io/github/lluaslua/todolist_api/service/TasksService.java
@@ -0,0 +1,40 @@
+package io.github.lluaslua.todolist_api.service;
+
+import io.github.lluaslua.todolist_api.model.Tasks;
+import io.github.lluaslua.todolist_api.repository.TasksRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RequiredArgsConstructor
+public class TasksService {
+
+    private final TasksRepository tasksRepository;
+
+    private void saveTask(Tasks task) {
+        tasksRepository.save(task);
+    }
+
+    public Tasks findById(long id){
+        return tasksRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Task not found with id " + id)
+        );
+    }
+
+    public void deleteTaskById(long id) {
+        tasksRepository.deleteById(id);
+    }
+
+    public void updateTaskById(Long id, Tasks task){
+        Tasks taskEntity = tasksRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found with id " + id));
+        Tasks updatedTask = Tasks.builder()
+            .id(taskEntity.getId())
+            .taskName(task.getTaskName() != null ? taskEntity.getTaskName() : taskEntity.getTaskName())
+            .taskPriority(taskEntity.getTaskPriority() != null ? taskEntity.getTaskPriority() : taskEntity.getTaskName())
+            .build();
+        tasksRepository.saveAndFlush(updatedTask);
+    }
+
+
+}
